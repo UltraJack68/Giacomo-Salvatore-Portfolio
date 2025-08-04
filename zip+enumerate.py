abde82515dfecc48e2adb6b860ad1cc4ae825d34
Jack <ultrajack68@gmail.com>
2025-08-04 11:17:30 +0200

    DSA

---
+++ zip+enumerate.py
@@ -0,0 +1,18 @@
+# %% cell 1
+nums = [1, 2, 3, 4, 5]
+nums1 = [6, 7, 8, 9, 0]
+
+def enumerates(nums):
+    for index, x in enumerate(nums):
+        print(f"{index}, {x}" )
+
+
+# %%
+nums = [1, 2, 3, 4, 5]
+nums1 = [6, 7, 8, 9, 0]
+def zips():
+    for x, y in zip(nums, nums1):
+        print(f"{x}, {y}")
+
+
+# %%
