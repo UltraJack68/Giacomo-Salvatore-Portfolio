54a552f2e39403e16da590b14f25b0152ee060a7
Jack <ultrajack68@gmail.com>
2025-08-24 22:54:30 +0200

    DSA

--- Middle of the Linked List.py
+++ Middle of the Linked List.py
@@ -1,3 +1,18 @@
 """Given the head of a singly linked list, return the middle node of the linked list.
 
 If there are two middle nodes, return the second middle node."""
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+         
+        slow = head
+        fast = head
+        
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        if fast.next:
+            return slow.next
+        
+        return slow
