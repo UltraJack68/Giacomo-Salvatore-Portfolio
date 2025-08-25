2bc9cf1acc9e1e3a7334b71431c1afcfc0a2672f
Jack <ultrajack68@gmail.com>
2025-08-25 19:03:42 +0200

    DSA

---
+++ Remove Duplicates from Sorted List.py
@@ -0,0 +1,13 @@
+"""Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well."""
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pointer = head
+    
+        while pointer and pointer.next:
+            while pointer.next and pointer.val == pointer.next.val:
+                pointer.next = pointer.next.next
+            pointer = pointer.next
+
+        return head
+
