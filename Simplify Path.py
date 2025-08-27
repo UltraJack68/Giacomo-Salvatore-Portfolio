45ccbf41a952f86b58ee55d37125cb847270eacb
Jack <ultrajack68@gmail.com>
2025-08-28 01:26:12 +0200

    DSA

---
+++ Simplify Path.py
@@ -0,0 +1,37 @@
+"""You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+
+The rules of a Unix-style file system are as follows:
+
+A single period '.' represents the current directory.
+A double period '..' represents the previous/parent directory.
+Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+The simplified canonical path should follow these rules:
+
+The path must start with a single slash '/'.
+Directories within the path must be separated by exactly one slash '/'.
+The path must not end with a slash '/', unless it is the root directory.
+The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+Return the simplified canonical path."""
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        stack = []
+        dir_name = ""
+
+        for char in path + "/":
+            if char == "/":
+                if dir_name == "" or dir_name == ".":
+                    pass
+                elif dir_name == "..":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(dir_name)
+                dir_name = ""
+            else:
+                dir_name += char
+
+        return "/" + "/".join(stack)
