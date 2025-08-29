b397dc40ba6c865a123b6f4efd60704cda3600b6
Jack Salvatore <ultrajack68@gmail.com>
2025-08-30 00:20:44 +0200

    DSA

--- Moving Average from Data Stream.py
+++ Moving Average from Data Stream.py
@@ -4,3 +4,31 @@ Implement the MovingAverage class:
 
 MovingAverage(int size) Initializes the object with the size of the window size.
 double next(int val) Returns the moving average of the last size values of the stream."""
+
+from collections import deque
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.queue = deque()
+        self.window_lenght = size
+        self.sum = 0
+        
+
+    def next(self, val: int) -> float:
+        self.queue.append(val)
+        self.sum += val
+        if len(self.queue) > self.window_lenght:
+            self.sum -= self.queue.popleft()
+            return float(self.sum / self.window_lenght)
+        else:
+            return float(self.sum / len(self.queue))
+                
+            
+            
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
